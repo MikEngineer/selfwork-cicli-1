9a17e0bb1fa1fce86a65c1a5e2be8983671e1d00
Michelangelo Ruisi <mruisi96@gmail.com>
2024-12-21 22:22:08 +0100

    terminato selfwork cicli 1

---
+++ main.js
@@ -0,0 +1,15 @@
+let num = 2;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
+
+// sarebbe possibile usare anche:
+
+// let numero = 2;
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(num + " x " + i + " = " + num*i) ;
+// }
+
+// ma sarebbe inutilmente lungo e macchinoso
